4ce626d2befa5aa488faeeb1e2746c5d3ba3d13e
windstamp <zklinsh@163.com>
2021-01-05 10:57:35 +0800

    Update  Dockerfile launch.sh, and add debug.sh

---
+++ PyTorch/Translation/Transformer/scripts/debug_fp32.sh
@@ -0,0 +1,28 @@
+export CUDA_VISIBLE_DEVICES=2
+
+RESULTS_DIR='/results'
+CHECKPOINTS_DIR='/results/checkpoints'
+STAT_FILE=${RESULTS_DIR}/run_log.json
+mkdir -p $CHECKPOINTS_DIR
+
+python /workspace/translation/train.py \
+  /data/wmt14_en_de_joined_dict \
+    --arch transformer_wmt_en_de_big_t2t \
+    --share-all-embeddings \
+    --optimizer adam \
+    --adam-betas '(0.9, 0.997)' \
+    --adam-eps "1e-9" \
+    --clip-norm 0.0 \
+    --lr-scheduler inverse_sqrt \
+    --warmup-init-lr 0.0 \
+    --warmup-updates 4000 \
+    --lr 0.0006 \
+    --min-lr 0.0 \
+    --dropout 0.1 \
+    --weight-decay 0.0 \
+    --criterion label_smoothed_cross_entropy \
+    --label-smoothing 0.1 \
+    --max-tokens 2560 \
+    --seed 1 \
+    --fuse-layer-norm \
+    --save-dir ./checkpoints
--- PyTorch/Translation/Transformer/scripts/docker/launch.sh
+++ PyTorch/Translation/Transformer/scripts/docker/launch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# nvcr.io/nvidia/pytorch:20.06-py3 
+
 CMD=${1:-/bin/bash}
-NV_VISIBLE_DEVICES=${2:-"0,1,2,3,4,5,6,7,8"}
+NV_VISIBLE_DEVICES=${2:-"all"}
 DOCKER_BRIDGE=${3:-"host"}
 
-nvidia-docker run -it --rm \
+nvidia-docker run -it --name zkl-transformer-torch-fp32-2 \
   --net=$DOCKER_BRIDGE \
   --shm-size=1g \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
-  -e NVIDIA_VISIBLE_DEVICES=${NV_VISIBLE_DEVICES} \
+  -v $PWD:/workspace/translation \
   -v $PWD/results:/results \
   -v $PWD/data:/data \
   transformer_pyt $CMD
